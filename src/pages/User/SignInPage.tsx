cc6fd87d497903b40a881994183196400285ee7b
AnhBinCoder <135632758+AnhBinCoder@users.noreply.github.com>
2024-06-19 15:04:59 +0700

    CSS Sign In Page

--- src/pages/User/SignInPage.tsx
+++ src/pages/User/SignInPage.tsx
@@ -1,48 +1,27 @@
-import { Divider } from "antd";
+
 import { FormSignIn } from "../../components";
 
-const SignInPage = () => {
+const SignInPage: React.FC= () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-300">
-      <div className="w-1/2 rounded-lg bg-slate-200 p-8">
-        <h2 className="mb-4 text-center text-2xl font-bold">Welcome Back</h2>
-        <p className="mb-8 text-center text-sm font-light">
-          Login To Your FPT Account!
-        </p>
-        <div className="my-4 flex cursor-pointer items-center justify-center">
-          <div className="flex h-10 w-10 items-center rounded-full bg-blue-700">
-            <i className="fa-brands fa-facebook-f m-auto"></i>
+    <section className="h-screen">
+      <div className="container h-full">
+        <div className="gap-8 flex flex-wrap items-center">
+          {/* <!-- Left column container with background--> */}
+          <div className="mb-12 md:mb-0 md:w-6/12 lg:w-6/12">
+            <img
+              src="/public/image/image4.png"
+              className="w-full"
+              alt="Phone image"
+            />
           </div>
-          <span className="ml-3 hover:text-amber-500">
-            Continue with Facebook
-          </span>
-        </div>
-        <div className="my-4 flex cursor-pointer items-center justify-center">
-          <div className="flex h-10 w-10 items-center rounded-full bg-amber-400">
-            <i className="fa-brands fa-google m-auto"></i>
+
+          {/* <!-- Right column container with form --> */}
+          <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
+            <FormSignIn />
           </div>
-          <span className="ml-3 hover:text-amber-500">
-            Continue with Google
-          </span>
         </div>
-        <FormSignIn />
-        <p className="text-center text-sm">
-          Or{" "}
-          <a href="#" className="text-amber-500 hover:underline">
-            Forgot password
-          </a>
-          .
-        </p>
-        <Divider />
-        <p className="text-center text-sm">
-          Don't have an account?{" "}
-          <a className="text-amber-500 hover:underline" href="/sign-up">
-            Sign Up
-          </a>
-        </p>
       </div>
-    </div>
+    </section>
   );
 };
-
 export default SignInPage;
