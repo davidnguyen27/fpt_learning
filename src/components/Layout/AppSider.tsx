893e696f73fec0fe923a748b8225ad7f60835137
AnhBinCoder <135632758+AnhBinCoder@users.noreply.github.com>
2024-06-23 14:20:44 +0700

    AppSider Update

--- src/components/Layout/AppSider.tsx
+++ src/components/Layout/AppSider.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { Divider, Menu } from "antd";
 import { Footer } from "antd/es/layout/layout";
 import SubMenu from "antd/es/menu/SubMenu";
@@ -5,28 +6,61 @@ import { useNavigate } from "react-router-dom";
 
 const AppSider: React.FC<{ className?: string }> = () => {
   const navigate = useNavigate();
+  const [openKeys, setOpenKeys] = useState<string[]>([]);
+  const [selectedKeys, setSelectedKeys] = useState<string[]>(["1"]);
+
+  const onOpenChange = (keys: string[]) => {
+    setOpenKeys(keys.length ? [keys[keys.length - 1]] : []);
+  };
+
+  const onSelect = ({ key }: { key: string }) => {
+    setSelectedKeys([key]);
+    navigateToPage(key);
+  };
+
+  const navigateToPage = (key: string) => {
+    switch (key) {
+      case "1":
+        navigate("/");
+        break;
+      case "2":
+        navigate("/about");
+        break;
+      case "9":
+        navigate("/settings-page");
+        break;
+      case "10":
+        navigate("/help-page");
+        break;
+      case "11":
+        navigate("/report-page");
+        break;
+      default:
+        break;
+    }
+  };
 
   return (
-    <div className="flex h-full flex-col">
+    <>
       <Menu
         mode="inline"
-        defaultSelectedKeys={["1"]}
-        defaultOpenKeys={["sub1"]}
-        className="flex-grow bg-slate-200 text-sm"
+        selectedKeys={selectedKeys}
+        openKeys={openKeys}
+        onOpenChange={onOpenChange}
+        onSelect={onSelect}
+        className="bg-slate-200 text-sm"
       >
         <Menu.Item
-          className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+          className="hover:rounded-none hover:bg-[#c2410c] hover:text-white"
           key="1"
           icon={<i className="fa-solid fa-house"></i>}
-          onClick={() => navigate("/")}
         >
           Home
         </Menu.Item>
         <Menu.Item
-          className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+          className="hover:rounded-none hover:bg-[#c2410c] hover:text-white"
           key="2"
           icon={<i className="fa-solid fa-address-card"></i>}
-          onClick={() => navigate("/about")}
         >
           About
         </Menu.Item>
@@ -36,74 +70,71 @@ const AppSider: React.FC<{ className?: string }> = () => {
           title="Categories"
         >
           <Menu.Item
-            className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+            className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
             key="3"
           >
             Web Development
           </Menu.Item>
           <Menu.Item
-            className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+            className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
             key="4"
           >
             Data & Analytics
           </Menu.Item>
           <Menu.Item
-            className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+            className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
             key="5"
           >
             Information Technology
           </Menu.Item>
           <Menu.Item
-            className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+            className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
             key="6"
           >
             Marketing
           </Menu.Item>
           <Menu.Item
-            className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+            className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
             key="7"
           >
             Office Productivity
           </Menu.Item>
           <Menu.Item
-            className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+            className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
             key="8"
           >
             Business
           </Menu.Item>
         </SubMenu>
         <Menu.Item
-          className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+          className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
           key="9"
           icon={<i className="fa-solid fa-gear"></i>}
-          onClick={() => navigate("/settings-page")}
         >
           Setting
         </Menu.Item>
         <Menu.Item
-          className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+          className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
           key="10"
           icon={<i className="fa-solid fa-circle-question"></i>}
-          onClick={() => navigate("/help-page")}
         >
           Help
         </Menu.Item>
         <Menu.Item
-          className="hover:rounded-none hover:bg-amber-500 hover:text-black"
+          className="hover:rounded-none hover:bg-[#ef4444] hover:text-white"
           key="11"
           icon={<i className="fa-solid fa-flag"></i>}
-          onClick={() => navigate("/report-page")}
         >
           Report
         </Menu.Item>
       </Menu>
       <Divider />
-      <Footer className="bg-slate-200 px-8 py-3 text-center">
+      <Footer className="bg-slate-200 px-8 py-0 text-center">
         <span className="text-xs font-light">
           Copyright by FPT Education @2024
         </span>
       </Footer>
-    </div>
+    </>
   );
 };
 
