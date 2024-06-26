df3f7fbd636ed35d6d262bcab1257e22f9eb13b6
AnhBinCoder <hungvinhjr6@gmail.com>
2024-06-26 09:11:30 +0700

    SessionManagement

--- src/pages/Instructor/SessionManagePage.tsx
+++ src/pages/Instructor/SessionManagePage.tsx
@@ -4,35 +4,25 @@ import { AppFooter, AppHeader } from "../../components";
 import Sider from "antd/es/layout/Sider";
 import { useSider } from "../../app/context/SiderContext";
 import SiderInstructor from "../../components/Instructor/SiderInstructor";
-import TableCourses from "../../components/Tables/TableCourses";
-import { useNavigate } from "react-router-dom";
+import { ExceptionOutlined } from "@ant-design/icons";
 import "../../styles/tabCustom.css";
+import TableSessions from "../../components/Tables/TableSessions";
 
 const items: TabsProps["items"] = [
   {
     key: "1",
     label: (
       <span className="text-sm font-medium">
-        <i className="fa-solid fa-book-open"></i> Courses
+        <ExceptionOutlined /> Sessions
       </span>
     ),
-    children: <TableCourses />,
-  },
-
-  {
-    key: "3",
-    label: (
-      <span className="text-sm font-medium">
-        <i className="fa-solid fa-up-long"></i> Upcoming Courses
-      </span>
-    ),
-    children: <TableCourses />,
+    children: <TableSessions />,
   },
 ];
 
 const SessionManagePage = () => {
   const { collapsed } = useSider();
-  const navigate = useNavigate();
+//   const navigate = useNavigate();
 
   return (
     <Layout className="flex h-screen w-screen flex-col">
@@ -55,29 +45,13 @@ const SessionManagePage = () => {
           <Content className="flex-1 overflow-y-auto">
             <div className="p-8">
               <section>
-                <h1 className="text-xl font-bold">Courses</h1>
+                <h1 className="text-xl font-bold">Session Management</h1>
               </section>
-              <div className="mt-4 flex items-center justify-between bg-slate-200 p-4">
-                <div className="flex items-center">
-                  <i className="fa-solid fa-book-open"></i>
-                  <span className="text-md ml-4 font-semibold">
-                    Jump Into Course Creation
-                  </span>
-                </div>
-                <div>
-                  <button
-                    className="rounded-lg bg-red-500 px-5 py-2 text-sm font-medium text-white hover:bg-red-200 hover:text-black"
-                    onClick={() => navigate("/create-course")}
-                  >
-                    Create Your Courses
-                  </button>
-                </div>
-              </div>
               <Tabs defaultActiveKey="1" items={items} className="mt-10" />
             </div>
             <Footer className="footer">
-              <AppFooter />
-            </Footer>
+            <AppFooter />
+          </Footer>
           </Content>
         </Layout>
       </Layout>
